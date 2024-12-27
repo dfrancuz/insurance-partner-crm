30c13027a4b60fd6a38f3bb9a4841f7f783c6494
Dejan Francuz <dejanfrancuz508@gmail.com>
2024-12-27 14:05:47 +0100

    chore: create base project code

---
+++ InsurancePartner.Web/InsurancePartner.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InsurancePartner.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
