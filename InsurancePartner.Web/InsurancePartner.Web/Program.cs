cce97f81e2c1349280f50536600963b9258da7b3
Dejan Francuz <dejanfrancuz508@gmail.com>
2025-01-02 21:33:41 +0100

    chore: reference data and logic layers

--- InsurancePartner.Web/InsurancePartner.Web/Program.cs
+++ InsurancePartner.Web/InsurancePartner.Web/Program.cs
@@ -1,15 +1,17 @@
+using InsurancePartner.Data.DependencyInjection;
+using InsurancePartner.Logic.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDataLayer(builder.Configuration);
+builder.Services.AddLogicLayer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
